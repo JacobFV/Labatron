0babb5b68bf6d5194958288ecf8911ed22bfb5ea
Jacob Valdez <jacobfv123@gmail.com>
2022-06-07 14:31:45 -0500

    found structured source code

---
+++ Labatron/Labatron/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labatron
+{
+    struct Answer
+    {
+        public string answer;
+        public string work;
+
+        public Answer(string answer, string work)
+        {
+            this.work = work;
+            this.answer = answer;
+        }
+    }
+}
---
+++ Labatron/Labatron/Givens.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labatron
+{
+    abstract class ReactionGiven
+    {
+        public virtual double moles { get; }
+        public virtual string moleCalc { get; }
+
+        public int numberGivenParams;
+        public ReactionCompound givenForCompound;
+        public double coefficentGiven;
+
+        internal ReactionGiven(double coefficentGiven, ReactionCompound givenForCompound)
+        {
+            this.givenForCompound = givenForCompound;
+            this.coefficentGiven = coefficentGiven;
+        }
+    }
+
+    class GramsGiven : ReactionGiven
+    {
+        public double gramsGiven;
+        public double gfwt;
+
+        public override double moles
+        {
+            get
+            {
+                return gramsGiven / gfwt;
+            }
+        }
+
+        public override string moleCalc
+        {
+            get
+            {
+                return "(" + gramsGiven + "g / "
+                    + gfwt + "g/mol) " + givenForCompound.formula;
+            }
+        }
+
+        public GramsGiven(double gramsGiven, double gfwt, double coefficentFrom,
+            ReactionCompound forCompound) : base(coefficentFrom, forCompound)
+        {
+            this.gramsGiven = gramsGiven;
+            this.gfwt = gfwt;
+
+            numberGivenParams = 1;
+        }
+    }
+
+    class MolarityGiven : ReactionGiven
+    {
+        public double molarity;
+        public double liters;
+
+        public override string moleCalc
+        {
+            get
+            {
+                if (liters < 0.01)
+                {
+                    return "(" + (liters * 1000) + "mL * " + molarity + "mol/L) * 1L / 1000mL"
+                        + givenForCompound.formula;
+                }
+                else
+                {
+                    return "(" + liters + "L * " + molarity + "mol/L) "
+                        + givenForCompound.formula;
+                }
+            }
+        }
+
+        public override double moles
+        {
+            get
+            {
+                return molarity * liters;
+            }
+        }
+
+        public MolarityGiven(double molarity, double liters, double coefficentFrom,
+            ReactionCompound compoundFrom) : base(coefficentFrom, compoundFrom)
+        {
+            this.molarity = molarity;
+            this.liters = liters;
+
+            numberGivenParams = 2;
+        }
+    }
+}
